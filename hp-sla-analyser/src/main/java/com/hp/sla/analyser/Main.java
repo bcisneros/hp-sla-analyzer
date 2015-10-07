6a1760131c816c83f38b32de6d36522700bdf02c
Benjamin Cisneros (benjamin.cisneros.barraza@hpe.com) <>
2015-10-07 14:32:00 -0500

    Create Main class and its test from scratch

---
+++ hp-sla-analyser/src/main/java/com/hp/sla/analyser/Main.java
@@ -0,0 +1,15 @@
+package com.hp.sla.analyser;
+
+/**
+ * Main class of the application. Contains the main method to execute the
+ * application
+ *
+ * @author Benjamin Cisneros Barraza
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Starting HP SLA Analyser");
+    }
+
+}
