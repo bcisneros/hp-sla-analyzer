c274c6535f1fc9f6e1eb9af7fa94a1b6ea669ec2
Benjamin Cisneros (benjamin.cisneros.barraza@hpe.com) <>
2015-10-08 12:56:52 -0500

    Create SlaReportGenerator class with dummy implementation

---
+++ hp-sla-analyser/src/main/java/com/hp/sla/analyser/model/SlaReportGenerationException.java
@@ -0,0 +1,13 @@
+package com.hp.sla.analyser.model;
+
+/**
+ *
+ * @author Benjamin Cisneros Barraza
+ */
+class SlaReportGenerationException extends Exception {
+
+    SlaReportGenerationException(String message) {
+        super(message);
+    }
+
+}
---
+++ hp-sla-analyser/src/main/java/com/hp/sla/analyser/model/SlaReportGenerator.java
@@ -0,0 +1,14 @@
+package com.hp.sla.analyser.model;
+
+/**
+ *
+ * @author Benjamin Cisneros Barraza
+ */
+public class SlaReportGenerator {
+
+    public void generateReport(String incidentFile, String auditFile, String d)
+            throws SlaReportGenerationException {
+        throw new SlaReportGenerationException("Not supported yet");
+    }
+
+}
