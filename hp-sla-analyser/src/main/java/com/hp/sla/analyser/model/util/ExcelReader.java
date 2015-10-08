57c5e6c4676198ae8228488f84ea126ebaaafe41
Benjamin Cisneros (benjamin.cisneros.barraza@hpe.com) <>
2015-10-08 12:08:30 -0500

    #2 Create ExcelReader class and test its functionality. Remove some code from other classes.

---
+++ hp-sla-analyser/src/main/java/com/hp/sla/analyser/model/util/ExcelReader.java
@@ -0,0 +1,62 @@
+package com.hp.sla.analyser.model.util;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+/**
+ *
+ * @author Benjamin Cisneros Barraza
+ */
+public class ExcelReader {
+
+    private FileInputStream inputFile;
+
+    public void setInputFile(FileInputStream inputFile) {
+        this.inputFile = inputFile;
+    }
+
+    public void read() throws IOException {
+        Workbook w;
+
+        try {
+            XSSFWorkbook myWorkBook = new XSSFWorkbook(inputFile);
+            // Return first sheet from the XLSX workbook XSSFSheet mySheet = myWorkBook.getSheetAt(0);
+            XSSFSheet mySheet = myWorkBook.getSheetAt(0);
+            // Get iterator to all the rows in current sheet 
+            Iterator<Row> rowIterator = mySheet.iterator();
+            // Traversing over each row of XLSX file 
+            while (rowIterator.hasNext()) {
+                Row row = rowIterator.next();
+                // For each row, iterate through each columns 
+                Iterator<Cell> cellIterator = row.cellIterator();
+                while (cellIterator.hasNext()) {
+                    Cell cell = cellIterator.next();
+                    switch (cell.getCellType()) {
+                        case Cell.CELL_TYPE_STRING:
+                            System.out.print(cell.getStringCellValue() + "\t");
+                            break;
+                        case Cell.CELL_TYPE_NUMERIC:
+                            System.out.print(cell.getNumericCellValue() + "\t");
+                            break;
+                        case Cell.CELL_TYPE_BOOLEAN:
+                            System.out.print(cell.getBooleanCellValue() + "\t");
+                            break;
+                        default:
+                    }
+                }
+                System.out.println("");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ hp-sla-analyser/src/test/java/com/hp/sla/analyser/model/util/ExcelReaderTest.java
@@ -0,0 +1,53 @@
+package com.hp.sla.analyser.model.util;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Benjamin Cisneros Barraza
+ */
+public class ExcelReaderTest {
+    
+    public ExcelReaderTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+
+    /**
+     * Test of read method, of class ExcelReader.
+     */
+    @Test
+    public void testRead() throws Exception {
+        System.out.println("read");
+        ExcelReader excelReader = new ExcelReader();
+        ClassLoader classLoader = getClass().getClassLoader(); 
+        FileInputStream testFile = new FileInputStream(new File(classLoader.getResource("files/testWorkBook.xlsx").getFile()));
+        excelReader.setInputFile(testFile);
+        excelReader.read();
+    }
+    
+}
