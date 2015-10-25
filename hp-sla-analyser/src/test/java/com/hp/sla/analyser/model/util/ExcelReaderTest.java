063fdaa06196b6a3a14042d01123c7a247d42a71
Benjamin Cisneros <benjamin.cisneros.barraza@hpe.com>
2015-10-25 14:26:14 -0600

    Remove unused methods

--- hp-sla-analyser/src/test/java/com/hp/sla/analyser/model/util/ExcelReaderTest.java
+++ hp-sla-analyser/src/test/java/com/hp/sla/analyser/model/util/ExcelReaderTest.java
@@ -2,37 +2,15 @@ package com.hp.sla.analyser.model.util;
 
 import com.hp.sla.analyser.util.ResourcesUtil;
 import org.apache.poi.ss.usermodel.Workbook;
-import org.junit.After;
-import org.junit.AfterClass;
 import static org.junit.Assert.assertNotNull;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
  * Test class for ExcelReader class
+ *
  * @author Benjamin Cisneros Barraza
  */
 public class ExcelReaderTest {
-
-    public ExcelReaderTest() {
-    }
-
-    @BeforeClass
-    public static void setUpClass() {
-    }
-
-    @AfterClass
-    public static void tearDownClass() {
-    }
-
-    @Before
-    public void setUp() {
-    }
-
-    @After
-    public void tearDown() {
-    }
 
     @Test
     public void testRead() throws Exception {
