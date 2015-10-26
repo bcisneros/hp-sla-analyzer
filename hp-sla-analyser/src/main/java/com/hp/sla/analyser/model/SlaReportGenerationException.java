c24a67bdc02d148734f3657f17d38d229a06999e
Benjamin Cisneros <benjamin.cisneros.barraza@hpe.com>
2015-10-26 13:27:36 -0600

    #29 Add class and constructor comments

--- hp-sla-analyser/src/main/java/com/hp/sla/analyser/model/SlaReportGenerationException.java
+++ hp-sla-analyser/src/main/java/com/hp/sla/analyser/model/SlaReportGenerationException.java
@@ -1,11 +1,19 @@
 package com.hp.sla.analyser.model;
 
 /**
+ * Represents an error during the process of report generation.
  *
  * @author Benjamin Cisneros Barraza
  */
 public class SlaReportGenerationException extends Exception {
 
+    private static final long serialVersionUID = 3226961927176235765L;
+
+    /**
+     * Constructor who takes a String and call super class constructor
+     *
+     * @param message
+     */
     SlaReportGenerationException(String message) {
         super(message);
     }
