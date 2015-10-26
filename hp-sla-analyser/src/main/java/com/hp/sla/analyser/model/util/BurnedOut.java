f1bc82c1be464e94d75f085e79cc5a1e95967f0e
ramirmal <ramirmal@192.168.55.106>
2015-10-26 10:28:52 -0600

    Added enum classes

---
+++ hp-sla-analyser/src/main/java/com/hp/sla/analyser/model/util/BurnedOut.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hp.sla.analyser.model.util;
+
+/**
+ *
+ * @author ramirmal
+ */
+public enum BurnedOut {
+
+    COMPLIANCE("yes"),
+    NON_COMPLIANCE("no"),
+    UNDETERMINED("undetermined");
+
+    private String name;
+
+    BurnedOut(String name) {
+        this.name = name;
+    }
+
+}
---
+++ hp-sla-analyser/src/main/java/com/hp/sla/analyser/model/util/Priority.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hp.sla.analyser.model.util;
+
+/**
+ * The kinds of priority of a ticket
+ *
+ * @author ramirmal
+ */
+public enum Priority {
+
+    TOP("top"),
+    HIGH("high"),
+    MEDIUM("medium"),
+    LOW("low");
+
+    private final String name;   // The string that represents the criticality
+
+    Priority(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
