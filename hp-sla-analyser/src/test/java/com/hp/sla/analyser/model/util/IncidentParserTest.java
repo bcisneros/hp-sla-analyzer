4a6b0435161eb58bef6ebdae94480b8657829061
Benjamin Cisneros <benjamin.cisneros.barraza@hpe.com>
2015-10-26 20:10:37 -0600

    #36 First testCreateObject method approach. (Needs a lot of improvement)

--- hp-sla-analyser/src/test/java/com/hp/sla/analyser/model/util/IncidentParserTest.java
+++ hp-sla-analyser/src/test/java/com/hp/sla/analyser/model/util/IncidentParserTest.java
@@ -1,11 +1,24 @@
 package com.hp.sla.analyser.model.util;
 
 import com.hp.sla.analyser.model.Incident;
+import com.hp.sla.analyser.model.TestIncidentBuilder;
+import com.hp.sla.analyser.util.DateTimeUtil;
 import com.hp.sla.analyser.util.ResourcesUtil;
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.xssf.usermodel.helpers.XSSFRowShifter;
+import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.fail;
+import org.junit.Test;
 
 /**
  * Test class for IncidentParser class
@@ -46,5 +59,43 @@ public class IncidentParserTest extends ExcelParserTest<Incident> {
     @Override
     protected ExcelParser getInstance() {
         return new IncidentParser();
+    }
+
+    @Test
+    public void testCreateObject() {
+        Incident incidentExpected = TestIncidentBuilder.getInstance().build();
+        IncidentParser incidentParser = (IncidentParser) getInstance();
+        Row row = new XSSFWorkbook().createSheet().createRow(0);
+        int i = 0;
+        row.createCell(i++).setCellValue(incidentExpected.getId());
+        row.createCell(i++).setCellValue(incidentExpected.getCreationTimestamp());
+        row.createCell(i++).setCellValue(incidentExpected.getCloseTimestamp());
+        row.createCell(i++).setCellValue(100);
+        row.createCell(i++).setCellValue("");
+        row.createCell(i++).setCellValue("");
+        row.createCell(i++).setCellValue("");
+        row.createCell(i++).setCellValue("");
+        row.createCell(i++).setCellValue(0);
+        row.createCell(i++).setCellValue("");
+        row.createCell(i++).setCellValue("");
+        row.createCell(i++).setCellValue("");
+        row.createCell(i++).setCellValue("");
+        row.createCell(i++).setCellValue("");
+        row.createCell(i++).setCellValue("");
+        row.createCell(i++).setCellValue("");
+        row.createCell(i++).setCellValue("");
+        row.createCell(i++).setCellValue("");
+        row.createCell(i++).setCellValue("");
+        row.createCell(i++).setCellValue("");
+        row.createCell(i++).setCellValue(0);
+        row.createCell(i++).setCellValue("");
+        row.createCell(i++).setCellValue("");
+        row.createCell(i++).setCellValue("");
+        row.createCell(i++).setCellValue("");
+        row.createCell(i++).setCellValue("");
+        Iterator<Cell> iterator = row.iterator();
+        Incident incident = incidentParser.createObject(iterator);
+        assertNotNull(incident);
+        assertEquals(incidentExpected, incident);
     }
 }
